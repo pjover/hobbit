38424b1c1f2d0020e631f51d5b949e1e829f9595
Pere Jover <pjover@gmail.com>
2020-10-21 18:18:06 +0200

    Product and ProductRepository

---
+++ src/main/kotlin/cat/hobbiton/hobbit/db/repository/ProductRepository.kt
@@ -0,0 +1,6 @@
+package cat.hobbiton.hobbit.db.repository
+
+import cat.hobbiton.hobbit.domain.Product
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface ProductRepository : MongoRepository<Product, String>
