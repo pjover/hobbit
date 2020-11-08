3f2ed01cdaa6f587953028b75315c57e435fd75f
Pere Jover <pjover@gmail.com>
2020-11-08 19:08:19 +0100

    CustomizedCustomerRepositoryImpl: incrementId

--- src/main/kotlin/cat/hobbiton/hobbit/db/repository/CustomizedCustomerRepositoryImpl.kt
+++ src/main/kotlin/cat/hobbiton/hobbit/db/repository/CustomizedCustomerRepositoryImpl.kt
@@ -2,10 +2,13 @@ package cat.hobbiton.hobbit.db.repository
 
 import cat.hobbiton.hobbit.domain.Child
 import cat.hobbiton.hobbit.domain.Customer
+import cat.hobbiton.hobbit.domain.Sequence
+import cat.hobbiton.hobbit.domain.SequenceType
 import cat.hobbiton.hobbit.util.Logging
 import org.springframework.data.mongodb.core.MongoTemplate
 
 class CustomizedCustomerRepositoryImpl(
+        private val sequenceRepository: SequenceRepository,
         private val mongoTemplate: MongoTemplate
 ) : CustomizedCustomerRepository {
 
@@ -14,8 +17,22 @@ class CustomizedCustomerRepositoryImpl(
 
     override fun save(customer: Customer): Customer {
 
-        return completeChildrenCodes(customer)
+        val withId = incrementId(customer)
+        return completeChildrenCodes(withId)
+
+
 //        mongoTemplate.insert(customer, "customer")
+    }
+
+    private fun incrementId(customer: Customer): Customer {
+        if (customer.id != 0) return customer
+
+        val currSequence = sequenceRepository.findById(SequenceType.CUSTOMER).orElse(Sequence(SequenceType.CUSTOMER, 0))
+        val newSequence = Sequence(SequenceType.CUSTOMER, currSequence.counter + 1)
+        sequenceRepository.save(newSequence)
+        logger.debug("Updated customer sequence to ${newSequence.counter}")
+
+        return customer.copy(id = newSequence.counter)
     }
 
     private fun completeChildrenCodes(customer: Customer): Customer {
@@ -23,9 +40,13 @@ class CustomizedCustomerRepositoryImpl(
         val currChildren = customer.children.sortedBy { it.birthDate }
         val newChildren = mutableListOf<Child>()
         for (i in currChildren.indices) {
-            newChildren.add(currChildren[i].copy(
-                    code = customer.id * 10 + i
-            ))
+            if (currChildren[i].code == 0) {
+                newChildren.add(currChildren[i].copy(
+                        code = customer.id * 10 + i
+                ))
+            } else {
+                newChildren.add(currChildren[i])
+            }
         }
         return customer.copy(children = newChildren)
     }
--- src/test/kotlin/cat/hobbiton/hobbit/db/repository/CustomizedCustomerRepositoryImplTest.kt
+++ src/test/kotlin/cat/hobbiton/hobbit/db/repository/CustomizedCustomerRepositoryImplTest.kt
@@ -1,38 +1,73 @@
 package cat.hobbiton.hobbit.db.repository
 
+import cat.hobbiton.hobbit.domain.Sequence
+import cat.hobbiton.hobbit.domain.SequenceType
 import cat.hobbiton.hobbit.testChild1
 import cat.hobbiton.hobbit.testChild2
 import cat.hobbiton.hobbit.testCustomer
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.DescribeSpec
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.springframework.data.mongodb.core.MongoTemplate
+import java.util.*
 
 class CustomizedCustomerRepositoryImplTest : DescribeSpec() {
 
     init {
+        val sequenceRepository = mockk<SequenceRepository>()
         val mongoTemplate = mockk<MongoTemplate>()
-        val sut = CustomizedCustomerRepositoryImpl(mongoTemplate)
+        val sut = CustomizedCustomerRepositoryImpl(sequenceRepository, mongoTemplate)
 
         describe("save") {
 
             context("completes the children codes") {
 
-                val actual = sut.save(testCustomer().copy(
-                        children = listOf(
-                                testChild1().copy(code = 0),
-                                testChild2().copy(code = 0)
-                        )
-                ))
+                context("updates the new children codes") {
 
-                it("replaces the children codes") {
-                    actual.children[0].code shouldBe 1850
-                    actual.children[1].code shouldBe 1851
+                    val actual = sut.save(testCustomer().copy(
+                            children = listOf(
+                                    testChild1().copy(code = 0),
+                                    testChild2().copy(code = 0)
+                            )
+                    ))
+
+                    it("replaces the children codes") {
+                        actual.children[0].code shouldBe 1850
+                        actual.children[1].code shouldBe 1851
+                    }
                 }
+
+                context("preserves the old children codes") {
+
+                    val actual = sut.save(testCustomer())
+
+                    it("replaces the children codes") {
+                        actual.children[0].code shouldBe 1
+                        actual.children[1].code shouldBe 2
+                    }
+                }
+
             }
 
-            context("incrments the customer id") {
+            context("increments the customer id") {
 
+                every { sequenceRepository.findById(SequenceType.CUSTOMER) } returns Optional.of(Sequence(SequenceType.CUSTOMER, 999))
+                every { sequenceRepository.save(any()) } answers { firstArg() }
+
+                val actual = sut.save(testCustomer(id = 0))
+
+                it("updates the sequence") {
+                    actual.id shouldBe 1000
+                }
+
+                it("call collaborators") {
+                    verify(exactly = 1) {
+                        sequenceRepository.findById(SequenceType.CUSTOMER)
+                        sequenceRepository.save(Sequence(SequenceType.CUSTOMER, 1000))
+                    }
+                }
             }
 
             context("saves the customer") {
