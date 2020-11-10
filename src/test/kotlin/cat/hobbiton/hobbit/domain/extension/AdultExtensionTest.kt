860b93311f2d54d056d81d686850993090bc251f
Pere Jover <pjover@gmail.com>
2020-11-10 05:17:46 +0100

    validate at AdultExtension

--- src/test/kotlin/cat/hobbiton/hobbit/domain/extension/AdultExtensionTest.kt
+++ src/test/kotlin/cat/hobbiton/hobbit/domain/extension/AdultExtensionTest.kt
@@ -1,8 +1,10 @@
 package cat.hobbiton.hobbit.domain.extension
 
+import cat.hobbiton.hobbit.testAddress
 import cat.hobbiton.hobbit.testAdultMother
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.DescribeSpec
+import kotlin.test.assertFailsWith
 
 
 class AdultExtensionTest : DescribeSpec() {
@@ -51,6 +53,64 @@ class AdultExtensionTest : DescribeSpec() {
 
                     it("returns the correct text") {
                         actual shouldBe "Joana Bibiloni"
+                    }
+                }
+            }
+        }
+
+        describe("validate") {
+
+            context("valid") {
+
+                sut.validate()
+
+                it("should not throw any error") {}
+            }
+
+            context("not valid") {
+
+                context("Adult with blank name") {
+                    val executor = {
+                        sut.copy(name = "").validate()
+                    }
+
+                    it("throws an error") {
+                        val exception = assertFailsWith<IllegalArgumentException> { executor.invoke() }
+                        exception.message shouldBe "Adult name cannot be blank"
+                    }
+                }
+
+                context("Adult with blank surname") {
+                    val executor = {
+                        sut.copy(surname = "").validate()
+                    }
+
+                    it("throws an error") {
+                        val exception = assertFailsWith<IllegalArgumentException> { executor.invoke() }
+                        exception.message shouldBe "Adult surname cannot be blank"
+                    }
+                }
+
+                context("Adult with invalid tax id") {
+                    val executor = {
+                        sut.copy(taxId = "01960447X").validate()
+                    }
+
+                    it("throws an error") {
+                        val exception = assertFailsWith<IllegalArgumentException> { executor.invoke() }
+                        exception.message shouldBe "Adult tax id 01960447X is not valid"
+                    }
+                }
+
+                context("Adult with invalid address") {
+
+                    val executor = {
+                        sut.copy(address = testAddress().copy(street = "")).validate()
+                    }
+
+                    it("throws an error") {
+                        val exception = assertFailsWith<IllegalArgumentException> { executor.invoke() }
+                        exception.message shouldBe "Address street cannot be blank"
                     }
                 }
             }
