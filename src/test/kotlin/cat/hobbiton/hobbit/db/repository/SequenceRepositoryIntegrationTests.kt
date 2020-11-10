c707d974c10f4520e743da464b1cccae5ea97575
Pere Jover <pjover@gmail.com>
2020-11-10 06:25:09 +0100

    Restored SequenceRepositoryIntegrationTests

---
+++ src/test/kotlin/cat/hobbiton/hobbit/db/repository/SequenceRepositoryIntegrationTests.kt
@@ -0,0 +1,32 @@
+package cat.hobbiton.hobbit.db.repository
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.http.HttpStatus
+
+//@DataMongoTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class SequenceRepositoryIntegrationTests {
+
+    @LocalServerPort
+    private val port = 0
+
+    @Autowired
+    private val restTemplate: TestRestTemplate? = null
+
+    @Test
+    @Throws(Exception::class)
+    fun sequences() {
+        val response = restTemplate?.getForEntity(
+                "http://localhost:$port/sequences?page=0&size=2",
+                String::class.java)!!
+
+        assertThat(response.statusCode, equalTo(HttpStatus.OK))
+    }
+}
