920bd71b04909635ffdd98c19db5ab2232ca4880
Pere Jover <pjover@gmail.com>
2020-11-10 05:18:02 +0100

    validate at ChildExtension

--- src/main/kotlin/cat/hobbiton/hobbit/domain/extension/ChildExtension.kt
+++ src/main/kotlin/cat/hobbiton/hobbit/domain/extension/ChildExtension.kt
@@ -1,10 +1,32 @@
 package cat.hobbiton.hobbit.domain.extension
 
 import cat.hobbiton.hobbit.domain.Child
+import cat.hobbiton.hobbit.domain.GroupType
+import cat.hobbiton.hobbit.messages.ValidationMessages
+import cat.hobbiton.hobbit.util.translate
 
 fun Child.shortName(): String = "$name $surname"
 
 fun Child.completeName(): String {
     return if (secondSurname.isNullOrBlank()) shortName()
     else "${shortName()} $secondSurname"
+}
+
+fun Child.validate() {
+
+    require(name.isNotBlank()) {
+        ValidationMessages.ERROR_CHILD_NAME_BLANK.translate()
+    }
+
+    require(surname.isNotBlank()) {
+        ValidationMessages.ERROR_CHILD_SURNAME_BLANK.translate()
+    }
+
+    if (!taxId.isNullOrBlank()) require(taxId.isValidTaxId()) {
+        ValidationMessages.ERROR_CHILD_TAX_ID_INVALID.translate(taxId)
+    }
+
+    require(group != GroupType.UNDEFINED) {
+        ValidationMessages.ERROR_CHILD_GROUP_UNDEFINED.translate()
+    }
 }
--- src/test/kotlin/cat/hobbiton/hobbit/domain/extension/ChildExtensionTest.kt
+++ src/test/kotlin/cat/hobbiton/hobbit/domain/extension/ChildExtensionTest.kt
@@ -1,8 +1,10 @@
 package cat.hobbiton.hobbit.domain.extension
 
+import cat.hobbiton.hobbit.domain.GroupType
 import cat.hobbiton.hobbit.testChildren1
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.DescribeSpec
+import kotlin.test.assertFailsWith
 
 
 class ChildExtensionTest : DescribeSpec() {
@@ -51,6 +53,65 @@ class ChildExtensionTest : DescribeSpec() {
 
                     it("returns the correct text") {
                         actual shouldBe "Laura Llull"
+                    }
+                }
+            }
+        }
+
+        describe("validate") {
+
+            context("valid") {
+
+                sut.validate()
+
+                it("should not throw any error") {}
+
+            }
+
+            context("not valid") {
+
+
+                context("Child with blank name") {
+                    val executor = {
+                        sut.copy(name = "").validate()
+                    }
+
+                    it("throws an error") {
+                        val exception = assertFailsWith<IllegalArgumentException> { executor.invoke() }
+                        exception.message shouldBe "Child name cannot be blank"
+                    }
+                }
+
+                context("Child with blank surname") {
+                    val executor = {
+                        sut.copy(surname = "").validate()
+                    }
+
+                    it("throws an error") {
+                        val exception = assertFailsWith<IllegalArgumentException> { executor.invoke() }
+                        exception.message shouldBe "Child surname cannot be blank"
+                    }
+                }
+
+                context("Child with invalid tax id") {
+                    val executor = {
+                        sut.copy(taxId = "01960447X").validate()
+                    }
+
+                    it("throws an error") {
+                        val exception = assertFailsWith<IllegalArgumentException> { executor.invoke() }
+                        exception.message shouldBe "Child tax id 01960447X is not valid"
+                    }
+                }
+
+                context("Child with invalid group") {
+                    val executor = {
+                        sut.copy(group = GroupType.UNDEFINED).validate()
+                    }
+
+                    it("throws an error") {
+                        val exception = assertFailsWith<IllegalArgumentException> { executor.invoke() }
+                        exception.message shouldBe "Child group must be defined"
                     }
                 }
             }
