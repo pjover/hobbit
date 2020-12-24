310de05dcb76278262fefd3ef8abc9952c3aaf28
Pere Jover <pjover@gmail.com>
2020-12-24 10:02:31 +0100

    MonthReport: PoiSpreadSheetBuilderService.generate()

--- src/main/kotlin/cat/hobbiton/hobbit/service/generate/spreadsheet/poi/PoiSpreadSheetBuilderService.kt
+++ src/main/kotlin/cat/hobbiton/hobbit/service/generate/spreadsheet/poi/PoiSpreadSheetBuilderService.kt
@@ -1,18 +1,51 @@
 package cat.hobbiton.hobbit.service.generate.spreadsheet.poi
 
 import cat.hobbiton.hobbit.messages.ErrorMessages
-import cat.hobbiton.hobbit.service.generate.spreadsheet.SpreadSheet
-import cat.hobbiton.hobbit.service.generate.spreadsheet.SpreadSheetBuilderService
+import cat.hobbiton.hobbit.service.generate.spreadsheet.*
 import cat.hobbiton.hobbit.util.error.AppException
 import cat.hobbiton.hobbit.util.resource.FileResource
+import org.apache.poi.ss.util.CellReference
+import org.apache.poi.xssf.usermodel.*
 import org.springframework.stereotype.Service
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.util.*
 
 @Service
 class PoiSpreadSheetBuilderService : SpreadSheetBuilderService {
 
+    private lateinit var workbook: XSSFWorkbook
+    private lateinit var sheet: XSSFSheet
+    private lateinit var dateStyle: XSSFCellStyle
+    private lateinit var currencyStyle: XSSFCellStyle
+    private lateinit var data: SpreadSheet
+
     override fun generate(spreadSheet: SpreadSheet): FileResource {
+        init(spreadSheet)
+
+        titleRow()
+        dataRows()
+        formatSheet()
+
+        return getFileResource()
+    }
+
+    private fun getFileResource(): FileResource {
+        ByteArrayOutputStream().use {
+            workbook.write(it)
+            workbook.close()
+            return FileResource(it.toByteArray(), data.filename)
+        }
+    }
+
+    private fun init(spreadSheet: SpreadSheet) {
         validate(spreadSheet)
-        return FileResource(ByteArray(33), spreadSheet.filename)
+        this.data = spreadSheet
+        this.workbook = XSSFWorkbook()
+        this.sheet = workbook.createSheet(spreadSheet.title)
+        this.dateStyle = loadDateStyle()
+        this.currencyStyle = loadCurrencyStyle()
+        initTable()
     }
 
     private fun validate(spreadSheet: SpreadSheet) {
@@ -26,6 +59,102 @@ class PoiSpreadSheetBuilderService : SpreadSheetBuilderService {
         val columnCount = spreadSheet.headers.size
         if(maxCols != columnCount || minCols != columnCount) {
             throw AppException(ErrorMessages.ERROR_SPREAD_SHEET_NO_MATCH_COLUMN_NUMBER)
+        }
+    }
+
+    private fun loadTableStyle(style: XSSFTableStyleInfo, styleName: String) {
+        style.name = styleName
+        style.isShowColumnStripes = false
+        style.isShowRowStripes = true
+        style.setFirstColumn(false)
+        style.setLastColumn(false)
+    }
+
+    private fun loadDateStyle(): XSSFCellStyle {
+        val style = workbook.createCellStyle()
+        style.dataFormat = workbook.creationHelper.createDataFormat().getFormat("d-mmm-yy") // TODO from configuration
+        return style
+    }
+
+    private fun loadCurrencyStyle(): XSSFCellStyle {
+        val style = workbook.createCellStyle()
+        style.dataFormat = workbook.creationHelper.createDataFormat().getFormat("#,##0.00 €") // TODO from configuration
+        return style
+    }
+
+    private fun initTable() {
+
+        val table = sheet.createTable(workbook.creationHelper.createAreaReference(
+            CellReference(0, 0),
+            CellReference(dataLinesCount(), columnCount() - 1)))
+
+        table.name = filenameWithoutExtension()
+        table.displayName = table.name
+
+        table.ctTable.addNewTableStyleInfo()
+        table.ctTable.tableStyleInfo.name = "TableStyleMedium4" // TODO from configuration
+        table.ctTable.addNewAutoFilter()
+
+        loadTableStyle(table.style as XSSFTableStyleInfo, table.ctTable.tableStyleInfo.name)
+    }
+
+    private fun columnCount() = data.headers.size
+
+    private fun dataLinesCount() = data.lines.size
+
+    private fun filenameWithoutExtension() = File(data.filename).nameWithoutExtension
+
+    private fun titleRow() {
+        val row = sheet.createRow(0)
+        for(i in 0 until columnCount()) {
+            val cell = row.createCell(i)
+            cell.setCellValue(data.headers[i])
+        }
+    }
+
+    private fun dataRows() {
+        for(i in data.lines.indices) dataRow(i + 1, data.lines[i])
+    }
+
+    private fun dataRow(rowNumber: Int, line: List<Cell>) {
+        val row = sheet.createRow(rowNumber)
+        for(i in line.indices) {
+            dataCell(row, i, line[i])
+        }
+    }
+
+    private fun dataCell(row: XSSFRow, colNumber: Int, dataCell: Cell) {
+        val cell = row.createCell(colNumber)
+        when(dataCell) {
+            is TextCell -> textCell(cell, dataCell.cellValue)
+            is DateCell -> dateCell(cell, dataCell.cellValue)
+            is IntCell -> numberCell(cell, dataCell.cellValue)
+            is DecimalCell -> numberCell(cell, dataCell.cellValue)
+            is CurrencyCell -> currencyCell(cell, dataCell.cellValue)
+        }
+    }
+
+    private fun textCell(cell: XSSFCell, cellValue: String) {
+        cell.setCellValue(cellValue)
+    }
+
+    private fun dateCell(cell: XSSFCell, cellValue: Date) {
+        cell.setCellValue(cellValue)
+        cell.cellStyle = dateStyle
+    }
+
+    private fun numberCell(cell: XSSFCell, cellValue: Double) {
+        cell.setCellValue(cellValue)
+    }
+
+    private fun currencyCell(cell: XSSFCell, cellValue: Double) {
+        cell.setCellValue(cellValue)
+        cell.cellStyle = currencyStyle
+    }
+
+    private fun formatSheet() {
+        for(i in 0 until columnCount()) {
+            sheet.autoSizeColumn(i)
         }
     }
 }
--- src/test/kotlin/cat/hobbiton/hobbit/service/generate/spreadsheet/poi/PoiSpreadSheetBuilderServiceTest.kt
+++ src/test/kotlin/cat/hobbiton/hobbit/service/generate/spreadsheet/poi/PoiSpreadSheetBuilderServiceTest.kt
@@ -7,9 +7,13 @@ import cat.hobbiton.hobbit.service.generate.spreadsheet.TextCell
 import cat.hobbiton.hobbit.util.error.AppException
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.DescribeSpec
+import org.apache.poi.ss.usermodel.WorkbookFactory
+import java.io.ByteArrayInputStream
 import java.math.BigDecimal
 import java.time.LocalDate
+import java.time.LocalDateTime
 import kotlin.test.assertFailsWith
+
 
 class PoiSpreadSheetBuilderServiceTest : DescribeSpec() {
 
@@ -213,6 +217,68 @@ class PoiSpreadSheetBuilderServiceTest : DescribeSpec() {
                     val exception = assertFailsWith<AppException> { executor.invoke() }
                     exception.message shouldBe "The number of headers colums does not match with the number of data columns"
                 }
+            }
+        }
+
+        describe("generate") {
+            val spreadSheetCells = SpreadSheet(
+                "test.xlsx",
+                "Test SpreadSheet",
+                listOf("Data",
+                    "Description",
+                    "Value"),
+                listOf(
+                    listOf(
+                        DateCell(LocalDate.of(2019, 5, 1)),
+                        TextCell("Line 1"),
+                        DecimalCell(BigDecimal.valueOf(11.5))),
+                    listOf(
+                        DateCell(LocalDate.of(2019, 5, 2)),
+                        TextCell("Line 2"),
+                        DecimalCell(BigDecimal.valueOf(22))))
+            )
+
+            val actual = sut.generate(spreadSheetCells)
+            val wb = WorkbookFactory.create(ByteArrayInputStream(actual.byteArray))
+            val numberOfSheets = wb.numberOfSheets
+            val sheet = wb.getSheetAt(0)
+            val sheetName = sheet.sheetName
+            val rowIterator = sheet.rowIterator()
+            val titles = rowIterator.next()
+            val titlescount = titles.count()
+            val title0 = titles.getCell(0).stringCellValue
+            val title1 = titles.getCell(1).stringCellValue
+            val title2 = titles.getCell(2).stringCellValue
+            val line1 = rowIterator.next()
+            val line1count = line1.count()
+            val cell10 = line1.getCell(0).localDateTimeCellValue
+            val cell11 = line1.getCell(1).stringCellValue
+            val cell12 = line1.getCell(2).numericCellValue
+            val line2 = rowIterator.next()
+            val line2count = line2.count()
+            val cell20 = line2.getCell(0).localDateTimeCellValue
+            val cell21 = line2.getCell(1).stringCellValue
+            val cell22 = line2.getCell(2).numericCellValue
+            val hasNext = rowIterator.hasNext()
+            wb.close()
+
+            it("generates a valid XLSX") {
+                actual.filename shouldBe "test.xlsx"
+                sheetName shouldBe "Test SpreadSheet"
+                numberOfSheets shouldBe 1
+                titlescount shouldBe 3
+                line1count shouldBe 3
+                line2count shouldBe 3
+                hasNext shouldBe false
+                title0 shouldBe "Data"
+                title1 shouldBe "Description"
+                title2 shouldBe "Value"
+                cell10 shouldBe LocalDateTime.of(2019, 5, 1, 0, 0, 0)
+                cell11 shouldBe "Line 1"
+                cell12 shouldBe 11.5
+                cell20 shouldBe LocalDateTime.of(2019, 5, 2, 0, 0, 0)
+                cell21 shouldBe "Line 2"
+                cell22 shouldBe 22.0
             }
         }
     }
