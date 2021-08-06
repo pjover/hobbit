39fb5ffb2124994aa24184204e0ef0f88d12c016
Pere Jover <pjover@gmail.com>
2021-08-06 17:16:09 +0200

    Show app version & banner on start up

--- src/main/kotlin/cat/hobbiton/hobbit/HobbitApplication.kt
+++ src/main/kotlin/cat/hobbiton/hobbit/HobbitApplication.kt
@@ -1,11 +1,23 @@
 package cat.hobbiton.hobbit
 
+import cat.hobbiton.hobbit.util.Logging
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
+import org.springframework.context.event.EventListener
+
 
 @SpringBootApplication
-open class HobbitApplication
+class HobbitApplication {
+    private val logger by Logging()
+
+    @EventListener(ApplicationReadyEvent::class)
+    fun doSomethingAfterStartup() {
+        logger.info("🟢 Started $appName v$appVersion")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<HobbitApplication>(*args)
 }
+
